5d82fd5eb9d4e8b1432679ed6c83ae839b2068e4
Brian A. Miller <brian.miller.coding@gmail.com>
2024-07-17 15:34:45 -0700

    Add method to ArrayUtils for printing array of strings in pretty format

--- src/dev/brianmiller/utils/ArrayUtils.java
+++ src/dev/brianmiller/utils/ArrayUtils.java
@@ -18,4 +18,15 @@ public class ArrayUtils {
         }
         System.out.println(" ]");
     }
+
+    public static void printArray(String[] arr) {
+        System.out.print("[ ");
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 0) {
+                System.out.print(", ");
+            }
+            System.out.print("\"" + arr[i] + "\"");
+        }
+        System.out.println(" ]");
+    }
 }
